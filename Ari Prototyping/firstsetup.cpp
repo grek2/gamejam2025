7552abd787dde544ab7cd29908738af6bb8d6304
Ari Snyder <arisnyder27@gmail.com>
2025-02-24 23:39:56 -0500

    added torches, lighting(kinda.   \n still a sinner tho, cuz on laptop and not using vim or neovim

--- Ari Prototyping/firstsetup.cpp
+++ Ari Prototyping/firstsetup.cpp
@@ -19,7 +19,7 @@ const int ASCHIID = 68;
 const int width = 10;
 const int height = 10;
 
-const int torchsize = 3;
+const int torchsize = 2;
 const int playlight = 1;
 
 
@@ -37,14 +37,46 @@ void drawOcean(char ocean[height][width]){
     
 
 }
-void drawOceanLighted(char ocean[height][width],std::array<int,2> torchmap[]){
-    
+void drawOceanLighted(char ocean[height][width],std::array<int,2> torchmap[],int numtorches){
+    for(int i = 0;i<height;i++){
+        std::cout << "\033[F\r";
+    }
+    for(int i = 0; i<height;(std::cout<<"\r\n")){
+        for(int i2 = 0;i2<width;i2++){
+            int light =0;
+            for(int i3=0;i3<torchsize;i3++){
+                int distance = (i-torchmap[i3][0])*(i-torchmap[i3][0])+(i2-torchmap[i3][1])*(i2-torchmap[i3][1]);
+                if(distance< (torchsize+1)*(torchsize+1) ){ light++;}//grey
+                if(distance< torchsize*torchsize){ light++;}//bright
+
+            }
+            switch(light){
+                case(0):
+                    std::cout<<DARKOCEAN;
+                    break;
+                case(1):
+                    std::cout<<GRAYEOCEAN;
+                    break;
+                default:                
+                    char c = ocean[i][i2];
+                    std::cout<< c;
+                    break;
+            }
+        }
+        i++;
+    }
 }
 
 //function to place torches. override later
-void torchoverride(std::array<int,2> torchmap[],int torchmaplength){
-    for(int i =1;i<torchmaplength;i++){
-        torchmap[i]={rand()%10,rand()%10};
+void torchoverride(std::array<int,2> torchmap[],int torchmaplength, char ocean[height][width]){
+    for(int i =0;i<torchmaplength;i++){
+        std::array<int,2> torch = {/*height*/rand()%10,/*width*/rand()%10};
+        while (ocean[torch[0]][torch[1]]!=' '){
+            std::cout<<"failed torch";
+            torch = {/*height*/rand()%10,/*width*/rand()%10};
+        }
+        ocean[torch[0]][torch[1]] = 'T';
+        torchmap[i]=torch;
     }
 }
 void genmap(char map[][width]){
@@ -64,8 +96,10 @@ int main(){
     }
     char oceanmap[height][width];
     genmap(oceanmap);
+    std::array<int,2> torchmap[3];
+    torchoverride(torchmap,3,oceanmap);
     
-    drawOcean(oceanmap);
+    drawOceanLighted(oceanmap,torchmap,3);
 
 
 }
